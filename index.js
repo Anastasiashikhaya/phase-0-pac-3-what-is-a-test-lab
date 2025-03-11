4839cc020ff431031bc7b9c2fd3ba23ba3b35b8e
Anastasia Wakoli <anastasiashikhaya@gmail.com>
2025-03-11 13:39:41 +0300

    test lab

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 30; // Should be less than 40
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
+
+
---
+++ test.js
@@ -0,0 +1,5 @@
+const { name, height, message } = require("./index");
+
+console.log(name);  // Output: Joe
+console.log(height); // Output: 74
+console.log(message); // Output: Joe is 74 inches tall
